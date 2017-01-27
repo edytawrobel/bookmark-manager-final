beadf03f2dc0e9ddc13c2f9905d71911ab8a9def
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-27 10:26:19 +0000

    Add password confirmation posibility. Challenge 19.

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,7 +1,14 @@
 feature 'User sign up' do
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
+
+  scenario 'wrong password confirmation raises error' do
+    wrong_sign_up
+    expect { wrong_sign_up }.not_to change(User, :count)
+  end
+
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -15,8 +15,16 @@ end
 
 def sign_up
   visit '/users/new'
-  expect(page.status_code).to eq(200)
   fill_in :email,    with: 'alice@example.com'
   fill_in :password, with: 'oranges!'
+  fill_in :password_confirmation, with: 'oranges!'
+  click_button 'Sign up'
+end
+
+def wrong_sign_up
+  visit '/users/new'
+  fill_in :email,    with: 'alice@example.com'
+  fill_in :password, with: 'oranges!'
+  fill_in :password_confirmation, with: 'wrong'
   click_button 'Sign up'
 end
